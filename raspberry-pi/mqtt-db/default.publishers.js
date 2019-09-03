971ebda294a4ef9d164d58af25cdff1511d6eaad
JohanWinther <johan.winther@outlook.com>
2019-09-03 21:16:28 +0200

    Adds username to path

--- raspberry-pi/mqtt-db/default.publishers.js
+++ raspberry-pi/mqtt-db/default.publishers.js
@@ -1,4 +1,4 @@
-module.exports.db_path = "~/db/publishers.db"; // Default location, don't change unless you know what you are doing
+module.exports.db_path = "/home/pi/db/publishers.db"; // Default location, don't change unless you know what you are doing
 module.exports.publishers = [
     {
         "db_table": "sql_table_name",
