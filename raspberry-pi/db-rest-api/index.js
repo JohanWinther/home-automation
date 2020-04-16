a4e001b5710d1a9d24aea0e619ff1e20456a89ff
Johan Winther <johan.winther@outlook.com>
2020-04-16 11:33:59 +0200

    Fix db-rest-api bug

--- raspberry-pi/db-rest-api/index.js
+++ raspberry-pi/db-rest-api/index.js
@@ -1,83 +1,101 @@
 const publishConfig = require("/home/pi/mqtt-db/publishers");
 let db_path = publishConfig.db_path;
 
-const express = require("express")
-const cors = require('cors')
-var app = express()
-app.use(cors())
+const express = require("express");
+const cors = require('cors');
+var app = express();
+app.use(cors());
 
-var spawn = require('child_process').spawn;
+// Server port
+const HTTP_PORT = 8081;
 
-let sqlite = (statement, table_id) => 
+const exec = require('child_process').exec;
+
+let sqlite = (statement, table_id) =>
     new Promise((resolve, reject) => {
-        let command = spawn(`sqlite3`, [db_path, statement])
+        let command = `sqlite3 ${db_path} "${statement}"`;
+        console.log(command);
 
-        var queryResult = ''
-        command.stdout.on('data', data => {
-            queryResult += data.toString()
-        })
+        exec(command, (error, stdout, stderr) => {
+            if (error === null) {
+                let result = stdout.split('\n').map(measurement => {
+                    [ts, val] = measurement.split('|');
+                    ts = parseInt(ts);
+                    ts *= 1000; // Unix time format
+                    return {
+                        "id": table_id,
+                        "timestamp": ts,
+                        "value": val, // parsing is handled by client
+                    }
+                });
+                result = result.filter(item => item.timestamp && item.value && item.id);
+                resolve(result);
+            } else {
+                error["stderr"] = stderr;
+                reject(error);
+            }
+        });
 
-        command.on('close', () => {
-            let result = queryResult.toString().split('\n').map(item => {
-                [ts, val] = item.split('|')
-                ts *= 1000
-                return {"timestamp": ts, "value": val, "id": table_id}
-            })
-            result = result.filter(item => item.timestamp && item.value && item.id)
-            resolve(result)
-        })
-                
-        command.on('error', function(data) {
-            reject(data)
-        })
 
-        setTimeout(function() {
+        setTimeout(() => {
             reject('Promise timed out.');
         }, 5000);
 
     });
 
-// Server port
-var HTTP_PORT = 8081
-// Start server
-app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
-});
 
 // Root endpoint
 app.use("/history/:table_id", (req, res, next) => {
+    let start, end;
 
-    let start = Math.floor(parseInt(req.query.start)/1000)
-    let end = Math.floor(parseInt(req.query.end)/1000)
-    if (start > end) [start, end] = [end, start]
-
-    let statement = `SELECT * FROM ${req.params.table_id}`
-
-    statement += (start | end) ? ` WHERE ` :''
-    let startStatement = !isNaN(start) ? `time > ${start}`:''
-    let endStatement = !isNaN(end) ? `time < ${end}`:''
-    if (start && end) {
-        statement += `${startStatement} AND ${endStatement}`
-        
-        let numberOfRows = (end-start)/5
-        const maximumRows = 10000 // ~ 0.3 seconds
-        let nth = Math.ceil(numberOfRows/maximumRows)
-        statement += ` AND ROWID % ${nth} = 0`
-    } else {
-        statement += `${startStatement}${endStatement}`
+    if (req.query.start !== undefined) {
+        // Remove milliseconds from unix time integer
+        start = Math.floor(parseInt(req.query.start) / 1000);
     }
-    statement += ';'
-    
+    if (req.query.end !== undefined) {
+        end = Math.floor(parseInt(req.query.end) / 1000);
+    }
+    if (start && end && start > end) {
+        [start, end] = [end, start];
+    }
+
+    // Build sql query
+    let statement = ["SELECT", "*", "FROM", req.params.table_id]; // SELECT * FROM table_id
+
+    if (start || end) {
+
+        statement.push('WHERE'); // SELECT * FROM table_id WHERE
+
+        let timeQuery = []
+        if (start) {
+            timeQuery.push(`time > ${start}`);
+        }
+        if (end) {
+            timeQuery.push(`time < ${end}`);
+        }
+
+        // If range is selected, show {maximumRows} over range
+        if (start && end) {
+            const numberOfRows = (end - start) / 5; // Time difference in seconds / 5 seconds
+            const maximumRows = 10000 // load time ~ 0.3 seconds
+            let everyNthRow = Math.ceil(numberOfRows / maximumRows);
+            timeQuery.push(`ROWID % ${everyNthRow} = 0`);
+        }
+        timeQuery = timeQuery.join(' AND ');
+
+        statement.push(timeQuery);
+
+    }
+    statement = statement.join(" ") + ";"; // SELECT * FROM table_id WHERE time > start AND time < end AND ROWID % everyNthRow = 0;
+
     sqlite(statement, req.params.table_id).then(result => {
-        res.setHeader('Content-Type', 'application/json')
-        res.json(result)
+        res.json(result);
     }).catch(err => {
-        res.setHeader('Content-Type', 'application/json')
-        res.json({"error": err})
-    })
-    return;
+        res.json({ "error": err });
+    });
 });
 
-app.use(function(req, res){
-    res.status(404);
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log(`Server running on port ${HTTP_PORT}`);
 });
